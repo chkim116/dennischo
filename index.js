7a4d29249cb279c906e157312f00ae26cf9aea84
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-28 12:22:00 +0900

    셀렉트 메뉴에 따른 이미지 반환 기능

--- index.js
+++ index.js
@@ -6,16 +6,29 @@
 const active = document.querySelector(".active");
 const selectedMenu = document.querySelector(".selected__menu");
 const selectedMenuList = document.querySelectorAll(".selected__menu");
+const selectedImgList = document.querySelectorAll(".selected__img-list");
 
 const selected = document.querySelector(".selected");
 
-console.log(selectedMenuList);
-
 selectedMenuList.forEach((list) => {
-  list.addEventListener("mouseover", () => {
-    selectedMenu.classList.remove("active");
-  });
-  list.addEventListener("mouseout", () => {
-    selectedMenu.classList.add("active");
+  list.addEventListener("mouseover", outActive);
+  list.addEventListener("mouseout", onActive);
+  list.addEventListener("mouseover", (list) => {
+    let filter = list.target.dataset.set;
+    selectedImgList.forEach((img) => {
+      if (filter === img.dataset.filter) {
+        img.classList.add("showing");
+      } else {
+        img.classList.remove("showing");
+      }
+    });
   });
 });
+
+function outActive() {
+  selectedMenu.classList.remove("active");
+}
+
+function onActive() {
+  selectedMenu.classList.add("active");
+}
