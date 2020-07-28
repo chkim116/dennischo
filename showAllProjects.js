a26bd2aa6809b824a6bb92fbd90d3108b99c2105
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-29 00:07:00 +0900

    윈도우 크기를 768px로 줄이면 자동으로 프로젝트 이미지 추가
    showAllProjects 파일을 수정

--- showAllProjects.js
+++ showAllProjects.js
@@ -2,17 +2,32 @@ const openSection = document.querySelector(".footer__nav");
 const section = document.querySelector("#section");
 const sectionTitle = document.querySelector(".section__title");
 
+let isSection = false;
+function onSectionBy768() {
+  if (768 >= innerWidth && isSection === false) {
+    section.style.display = "block";
+    isSection = true;
+    console.log("onSectionBy768", isSection);
+  }
+}
+
+window.addEventListener("resize", onSectionBy768);
+
 function onSection() {
-  section.style.display = "block";
+  if (isSection === false) {
+    section.style.display = "block";
+    isSection = true;
+    console.log("onSection", isSection);
+  }
 }
 
 openSection.addEventListener("click", onSection);
 
 function outSection() {
-  if (window.innerWidth < 768) {
-    section.style.display = "block";
-  } else {
+  if (isSection === true && window.innerWidth > 768) {
     section.style.display = "none";
+    isSection = false;
+    console.log("outsection", isSection);
   }
 }
 
