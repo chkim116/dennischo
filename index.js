6d15dbd78716ee95e38dac84c1260604506c98a7
chkim116 <66462726+chkim116@users.noreply.github.com>
2020-07-28 12:34:18 +0900

    셀렉티드바에서 벗어나면 첫번째 목록, 첫번째 사진 보여주게 하기

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-// -- 다음 데니스조사이트 자바스그립트
-// 셀렉트메뉴에 마우스를올렸을때
-// 필터 메서드를 이용하여
-// event.target.dataset == 이미지.target이 맞을때 반환하기.
-
 const active = document.querySelector(".active");
 const selectedMenu = document.querySelector(".selected__menu");
 const selectedMenuList = document.querySelectorAll(".selected__menu");
 const selectedImgList = document.querySelectorAll(".selected__img-list");
 
-const selected = document.querySelector(".selected");
-
 selectedMenuList.forEach((list) => {
   list.addEventListener("mouseover", outActive);
   list.addEventListener("mouseout", onActive);
+
   list.addEventListener("mouseover", (list) => {
     let filter = list.target.dataset.set;
     selectedImgList.forEach((img) => {
@@ -21,6 +15,12 @@ selectedMenuList.forEach((list) => {
       } else {
         img.classList.remove("showing");
       }
+    });
+  });
+  list.addEventListener("mouseout", (list) => {
+    selectedImgList.forEach((img) => {
+      img.classList.remove("showing");
+      selectedImgList[0].classList.add("showing");
     });
   });
 });
